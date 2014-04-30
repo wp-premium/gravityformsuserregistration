feb2939f621798e51186386b68bfc1471b2e354d
Remco Tolsma <info@remcotolsma.nl>
2014-04-30 09:28:46 +0200

    Added version 1.8.

---
+++ includes/pending_activations.php
@@ -0,0 +1,318 @@
+<?php
+
+class GFUserPendingActiviations {
+    
+    static $message;
+    static $errors;
+    
+    public static function display_page() {
+        ?>
+        
+        <style type="text/css">
+            .nav-tab-wrapper { margin: 0 0 10px !important; }
+            .message { margin: 5px 0 15px; padding: 0.5em 0.6em; border: 1px solid #E6DB55; }
+            .message.updated { background-color: #FFFFE0;  }
+            .message.error { background-color: #FFEBE8; border-color: #CC0000; }
+            .gfur-form-select { float: right; margin-top: -30px; }
+        </style>
+        
+        <div class="wrap">
+
+            <?php $form = rgget('form_id') ? RGFormsModel::get_form( rgget('form_id') ) : false; ?>
+
+            <div style="background: url('<?php echo GFUser::get_base_url() ?>/images/user-registration-icon-32.png') no-repeat;" id="icon-edit" class="icon32 icon32-posts-post"><br></div>
+            <h2>
+                <?php if( !$form ) {
+                    _e("Pending Activations", "gravityformsuserregistration");
+                } else {
+                    $form_link = '"<a href="' . admin_url( 'admin.php?page=gf_edit_forms&id=' . $form->id ) . '">' . $form->title . '</a>"';
+                    printf( __('Pending Activations for %s'), $form_link);
+                } ?>
+            </h2>
+
+            <div class="gfur-form-select">
+
+                <?php
+                $pending_activation_forms = GFUser::get_pending_activation_forms();
+                $pending_activation_url = admin_url('admin.php?page=gf_user_registration&view=pending_activations&form_id=');
+                ?>
+                <select onchange="document.location = '<?php echo $pending_activation_url; ?>' + this.value;">
+                    <option value=""><?php _e('Select a Form', 'gravityformsuserregistration'); ?></option>
+                    <option value="all"><?php _e('View All Pending Activations', 'gravityformsuserregistration'); ?>
+                    <optgroup label="<?php _e('Forms', 'gravityformsuserregistration'); ?>">
+                        <?php foreach( $pending_activation_forms as $form_obj ) { ?>
+                            <option value="<?php echo $form_obj->id; ?>"><?php echo $form_obj->title; ?></option>
+                        <?php } ?>
+                    </optgroup></option>
+                </select>
+
+            </div>
+
+            <?php
+            
+            if(rgpost('is_submit')) {
+                self::handle_submission();
+                if(self::$errors) { ?>
+                    <div class="message error"><p><?php echo self::$errors; ?></p></div>
+                <?php } else { ?>
+                    <div class="message updated"><p><?php echo self::$message; ?></p></div>
+                <?php }
+            }
+            
+            ?>
+            
+            <form id="list_form" method="post" action="">
+            
+                <?php
+                $table = new GFUserPendingActivationsList();
+                $table->prepare_items();
+                $table->display();
+                ?>
+                
+                <input type="hidden" name="is_submit" value="1" />
+                <input type="hidden" id="single_action" name="single_action" value="" />
+                <input type="hidden" id="item" name="item" value="" />
+                
+                <?php wp_nonce_field('action', 'action_nonce'); ?>
+                
+            </form>
+            
+        </div>
+        
+        <script type="text/javascript">
+        
+        function singleItemAction(action, activationKey) {
+            jQuery('#item').val(activationKey);
+            jQuery('#single_action').val(action);
+            jQuery('#list_form')[0].submit();
+        }
+        
+        </script>
+        
+        <?php
+    }
+    
+    public static function get_pending_activations($form_id, $args = array()) {
+        global $wpdb;
+        
+        if ($form_id == "all")
+        	$form_id = "";
+        
+        extract(wp_parse_args($args, array(
+            'order' => 'DESC',
+            'order_by' => 'registered',
+            'page' => 1,
+            'per_page' => 10,
+            'get_total' => false,
+            'lead_id' => false
+        )));
+        
+        if(!is_multisite()) {
+            require_once(GFUser::get_base_path() . '/includes/signups.php');
+            GFUserSignups::prep_signups_functionality();
+        }
+        
+        $where = array();
+        
+        if($form_id)
+            $where[] = $wpdb->prepare("l.form_id = %d", $form_id);
+        
+        if($lead_id)
+            $where[] = $wpdb->prepare("l.id = %d", $lead_id);
+        
+        $where[] = "s.active = 0";
+        $where = 'WHERE ' . implode(' AND ', $where);
+        
+        $order = $wpdb->prepare("ORDER BY %s %s", $order_by, $order);
+        $offset = ($page * $per_page) - $per_page;
+        $limit_offset = $get_total ? '' : "LIMIT $per_page OFFSET $offset";
+        $method = $get_total ? 'get_var' : 'get_results';
+        
+        if($form_id) {
+            
+            $select = $get_total ? 'SELECT count(s.activation_key)' : 'SELECT s.*' ;
+            $sql = "
+                $select FROM {$wpdb->prefix}rg_lead_meta lm
+                INNER JOIN {$wpdb->signups} s ON s.activation_key = lm.meta_value AND lm.meta_key = 'activation_key'
+                INNER JOIN {$wpdb->prefix}rg_lead l ON l.id = lm.lead_id
+                $where
+                $order
+                $limit_offset";
+            $results = $wpdb->$method($sql);
+                
+        } else {
+            
+            $select = $get_total ? 'SELECT count(s.activation_key)' : 'SELECT s.*' ;
+            $results = $wpdb->$method("
+                $select FROM $wpdb->signups s
+                $where
+                $order
+                $limit_offset"
+                );
+            
+        }
+        
+        return $results;
+    }
+    
+    private static function process_bulk_action() {
+        
+    }
+    
+    public static function handle_submission() {
+        
+        if(!wp_verify_nonce(rgpost('action_nonce'), 'action') && !check_admin_referer('action_nonce', 'action_nonce'))
+            die('You have failed...');
+        
+        require_once(GFUser::get_base_path() . '/includes/signups.php');
+        GFUserSignups::prep_signups_functionality();
+        
+        self::$errors = '';
+        self::$message = '';
+        
+        $action = rgpost('single_action');
+        $action = !$action ? rgpost('action') != -1 ? rgpost('action') : rgpost('action2') : $action;
+        
+        $items = rgpost('item') ? array(rgpost('item')) : rgpost('items');
+        
+        foreach($items as $key) {
+        
+            switch($action) {
+            case 'delete':
+                $success = GFUserSignups::delete_signup($key);
+                if($success) {
+                    self::$message = _n('Item deleted.', 'Items deleted.', count($items), 'graivtyformsuserregistration');
+                } else {
+                    self::$errors = _n('There was an issue deleting this item.', 'There was an issue deleting one or more selected items.', count($items), 'graivtyformsuserregistration');
+                }
+                break;
+            case 'activate':
+                $userdata = GFUserSignups::activate_signup($key);
+                if(!is_wp_error($userdata) && rgar($userdata, 'user_id')) {
+                    self::$message = _n('Item activated.', 'Items activated.', count($items), 'graivtyformsuserregistration');
+                } else {
+                    self::$errors = _n('There was an issue activating this item', 'There was an issue activating one or more selected items', count($items), 'graivtyformsuserregistration');
+                    if(is_wp_error($userdata)) {
+                        $errors = reset($userdata->errors);
+                        self::$errors .= ": " . $errors[0];
+                    }
+                }
+                break;
+            }
+        
+        }
+        
+    }
+    
+}
+
+require_once(ABSPATH . '/wp-admin/includes/class-wp-list-table.php');
+
+class GFUserPendingActivationsList extends WP_List_Table {
+    
+    var $_column_headers;
+    
+    function __construct() {
+        
+        $this->items = array();
+        $this->_column_headers = array(
+            array(
+                'cb' => '<input type="checkbox" />',
+                'form' => 'Form',
+                'user_login' => 'Username',
+                'email' => 'Email',
+                'date' => 'Sign Up Date',
+                ),
+            array(),
+            array()
+        );
+        
+        parent::__construct();
+        
+    }
+    
+    function prepare_items() {
+        
+        $items = array();
+        $forms = array();
+        $per_page = 10;
+        $page = rgget('paged') ? rgget('paged') : 1;
+        $pending_activations = GFUserPendingActiviations::get_pending_activations(rgget('form_id'), array('per_page' => $per_page, 'page' => $page));
+        $total_pending = GFUserPendingActiviations::get_pending_activations(rgget('form_id'), array('per_page' => $per_page, 'page' => $page, 'get_total' => true));
+        
+        foreach($pending_activations as $pending_activation) {
+            $signup_meta = unserialize($pending_activation->meta);
+            
+            $lead = RGFormsModel::get_lead(rgar($signup_meta, 'lead_id'));
+            
+            //if(!$lead)
+                //continue;
+            
+            $form_id = $lead['form_id'];
+            $form = rgar($forms, $form_id) ? rgar($forms, $form_id) : RGFormsModel::get_form_meta($form_id);
+            $forms[$form_id] = $form;
+            
+            $item = array();
+            $item['form'] = $form['title'];
+            $item['user_login'] = rgar($signup_meta, 'user_login');
+            $item['email'] = rgar($signup_meta, 'email');
+            $item['date'] = $lead['date_created'];
+            
+            // non-columns
+            $item['lead_id'] = $lead['id'];
+            $item['form_id'] = $form_id;
+            $item['activation_key'] = $pending_activation->activation_key;
+            
+            array_push($this->items, $item);
+               
+        }
+        
+        $this->set_pagination_args(array(
+            'total_items' => $total_pending,
+            'per_page' => $per_page,
+        ));
+        
+    }
+    
+    function column_default($item, $column_name) {
+        return rgar($item, $column_name);
+    }
+    
+    function column_cb($item) {
+        return '<input type="checkbox" name="items[]" value="' . $item['activation_key'] . '" />';
+    }
+    
+    function column_form($item) {
+        $str = '<strong>' . rgar($item, 'form') . '</strong>';
+        $str .= '
+            <div class="row-actions">
+                <span class="inline hide-if-no-js">
+                    <a title="Activate this sign up" href="javascript: if(confirm(\'' . __('Activate this sign up? ', 'gravityformsuserregistration') . __("\'Cancel\' to stop, \'OK\' to activate.", "gravityformsuserregistration") . '\')) { singleItemAction(\'activate\',\'' . $item['activation_key'] . '\'); } ">Activate</a> | 
+                </span>
+                <span class="inline hide-if-no-js">
+                    <a title="View the entry associated with this sign up" href="' . admin_url("admin.php?page=gf_entries&view=entry&id={$item['form_id']}&lid={$item['lead_id']}") . '">View Entry</a> | 
+                </span>
+                <span class="inline hide-if-no-js">
+                    <a title="Delete this sign up?" href="javascript: if(confirm(\'' . __('Delete this sign up? ', 'gravityformsuserregistration') . __("\'Cancel\' to stop, \'OK\' to delete.", "gravityformsuserregistration") . '\')) { singleItemAction(\'delete\',\'' . $item['activation_key'] . '\'); } ">Delete</a>
+                </span>
+            </div>';
+        
+        return $str;
+    }
+    
+    function column_date($item) {
+        return GFCommon::format_date(rgar($item, 'date'), false);
+    } 
+    
+    function get_bulk_actions() {
+        $actions = array(
+            'activate' => __('Activate', 'gravityformsuserregistration'),
+            'delete' => __('Delete', 'gravityformsuserregistration')
+            );
+
+        return $actions;
+    }
+    
+}
+
+?>
---
+++ includes/signups.php
@@ -0,0 +1,245 @@
+<?php
+
+class GFUserSignups {
+
+    public static function create_signups_table() {
+        require_once(ABSPATH . "wp-admin/includes/upgrade.php");
+
+        global $wpdb, $charset_collate;
+
+        self::add_signups_to_wpdb();
+
+        $ms_queries = "CREATE TABLE $wpdb->signups (
+                domain varchar(200) NOT NULL default '',
+                path varchar(100) NOT NULL default '',
+                title longtext NOT NULL,
+                user_login varchar(60) NOT NULL default '',
+                user_email varchar(100) NOT NULL default '',
+                registered datetime NOT NULL default '0000-00-00 00:00:00',
+                activated datetime NOT NULL default '0000-00-00 00:00:00',
+                active tinyint(1) NOT NULL default '0',
+                activation_key varchar(50) NOT NULL default '',
+                meta longtext,
+                KEY activation_key (activation_key),
+                KEY domain (domain)
+                ) $charset_collate;";
+
+        // now create table
+        dbDelta($ms_queries);
+    }
+
+    /**
+    * Add signups property to $wpdb object. Used by several MS functions.
+    */
+    private static function add_signups_to_wpdb() {
+        global $wpdb;
+        $wpdb->signups = $wpdb->prefix . 'signups';
+    }
+
+    public static function prep_signups_functionality() {
+
+        if(!is_multisite()) {
+
+            // require MS functions
+            require_once(ABSPATH . 'wp-includes/ms-functions.php');
+
+            // add $wpdb->signups property (accessed in various MS functions)
+            self::add_signups_to_wpdb();
+
+            // remove filter which checks for Network setting (not active on non-ms install)
+            remove_filter('option_users_can_register', 'users_can_register_signup_filter');
+
+        }
+
+        // signup: update the signup URL to GF's custom activation page
+        add_filter( 'wpmu_signup_user_notification_email', array( 'GFUserSignups', 'modify_signup_user_notification_message' ), 10, 4 );
+        add_filter( 'wpmu_signup_blog_notification_email', array( 'GFUserSignups', 'modify_signup_blog_notification_message' ), 10, 7 );
+
+        add_filter( 'wpmu_signup_user_notification', array( __class__, 'add_site_name_filter' ) );
+        add_filter( 'wpmu_signup_user_notification_subject', array( __class__, 'remove_site_name_filter' ) );
+
+        // signup: BP cancels default MS signup notification and replaces with its own; hook up to BP's custom notification hook
+        if( GFUser::is_bp_active() ) {
+            add_filter('bp_core_activation_signup_user_notification_message', array('GFUserSignups', 'modify_signup_user_notification_message'), 10, 4);
+            add_filter('bp_core_activation_signup_blog_notification_message', array('GFUserSignups', 'modify_signup_blog_notification_message'), 10, 7);
+        }
+
+    }
+
+    public static function modify_signup_user_notification_message($message, $user, $user_email, $key) {
+
+        $signup = GFSignup::get( $key );
+
+        // if no signup or config is set for manual activation, return false preventing signup notification from being sent to user
+        if( is_wp_error( $signup ) || $signup->get_activation_type() == 'manual' )
+            return false;
+
+        $url = add_query_arg(array('page' => 'gf_activation', 'key' => $key), get_site_url() . '/' );
+
+        // BP replaces URL before passing the message, get the BP activation URL and replace
+        if( GFUser::is_bp_active() ) {
+            $activate_url = esc_url(bp_get_activation_page() . "?key=$key");
+            $message = str_replace($activate_url, '%s', $message);
+        }
+
+        return sprintf($message, $url);
+    }
+
+    public static function modify_signup_blog_notification_message($message, $domain, $path, $title, $user, $user_email, $key) {
+
+        $signup = GFSignup::get( $key );
+
+        // if no signup or config is set for manual activation, return false preventing signup notification from being sent to user
+        if( is_wp_error( $signup ) || $signup->get_activation_type() == 'manual' )
+            return false;
+
+        $url = add_query_arg(array('page' => 'gf_activation', 'key' => $key), get_site_url());
+
+        // BP replaces URL before passing the message, get the BP activation URL and replace
+        if(GFUser::is_bp_active()) {
+            $activate_url = esc_url(bp_get_activation_page() . "?key=$key");
+            $message = str_replace($activate_url, '%s', $message);
+        }
+
+        return sprintf($message, $url, esc_url("http://{$domain}{$path}"), $key);
+    }
+
+    public static function add_site_name_filter( $return ) {
+        add_filter( 'site_option_site_name', array( __class__, 'modify_site_name' ) );
+        return $return;
+    }
+
+    public static function remove_site_name_filter( $return ) {
+        remove_filter( 'site_option_site_name', array( __class__, 'modify_site_name' ) );
+        return $return;
+    }
+
+    public static function modify_site_name( $site_name ) {
+
+        if( ! $site_name )
+            $site_name = get_site_option( 'blogname' );
+
+        return $site_name;
+    }
+
+    public static function add_signup_meta($lead_id, $activation_key) {
+        gform_update_meta($lead_id, 'activation_key', $activation_key);
+    }
+
+    public static function get_lead_activation_key($lead_id) {
+        global $wpdb;
+        return $wpdb->get_var($wpdb->prepare("SELECT meta_value FROM {$wpdb->prefix}rg_lead_meta WHERE lead_id = %d AND meta_key = 'activation_key'", $lead_id));
+    }
+
+    /**
+     * Activate a signup.
+     *
+     */
+    public static function activate_signup($key) {
+        global $wpdb, $current_site;
+
+        $blog_id = is_object($current_site) ? $current_site->id : false;
+        $signup = GFSignup::get( $key );
+
+        if( is_wp_error( $signup ) )
+            return $signup;
+
+        $user_id = username_exists( $signup->user_login );
+        if ($user_id){
+        	//username already exists, go ahead and mark signup activated and return error message
+        	$signup->set_as_activated();
+        	return new WP_Error('user_already_exists', __( 'That username is already activated.' ), $signup);
+        }
+        if (email_exists($signup->user_email)){
+        	//email address already exists, return error message
+        	return new WP_Error('email_already_exists', __( 'Sorry, that email address is already used!' ), $signup);
+        }
+
+        // unbind site creation from gform_user_registered hook, run it manually below
+        if(is_multisite())
+            remove_action( 'gform_user_registered' , array( 'GFUser', 'create_new_multisite' ) );
+        
+        $user_data = GFUser::create_user( $signup->lead, $signup->form, $signup->config);
+        $user_id = rgar($user_data, 'user_id');
+
+        if(!$user_id){
+            return new WP_Error('create_user', __('Could not create user'), $signup);
+		}
+
+        $signup->set_as_activated();
+          
+        do_action('gform_activate_user', $user_id, $user_data, $signup->meta);
+
+        if(is_multisite()) {
+            $ms_options = rgars( $signup->config, 'meta/multisite_options');
+            if($ms_options['create_site'])
+                $blog_id = GFUser::create_new_multisite($user_id, $signup->config, $signup->lead, $user_data['password']);
+        }
+
+        return array('user_id' => $user_id, 'password' => $user_data['password'], 'blog_id' => $blog_id);
+    }
+
+    public static function delete_signup($key) {
+        global $wpdb;
+        return $wpdb->query($wpdb->prepare("DELETE FROM $wpdb->signups WHERE activation_key = %s", $key));
+    }
+
+}
+
+/**
+* Create a signup object from a signup key.
+*/
+
+class GFSignup {
+
+    public $meta;
+    public $lead;
+    public $form;
+    public $config;
+
+    private $error;
+
+    function __construct( $signup ) {
+
+        // @alex: not sure this is a good thing to do?
+        foreach( $signup as $key => $value ) {
+            $this->$key = $value;
+        }
+
+        $this->meta = unserialize( $signup->meta );
+        $this->lead = RGFormsModel::get_lead( $this->meta['lead_id'] );
+        $this->form = RGFormsModel::get_form_meta( $this->lead['form_id'] );
+        $this->config = GFUser::get_active_config( $this->form, $this->lead );
+
+    }
+
+    public static function get( $key ) {
+        global $wpdb;
+
+        $signup = $wpdb->get_row( $wpdb->prepare("SELECT * FROM $wpdb->signups WHERE activation_key = %s", $key) );
+
+        if(empty($signup))
+            return new WP_Error( 'invalid_key', __( 'Invalid activation key.' ) );
+
+        if($signup->active)
+            return new WP_Error( 'already_active', __( 'The user is already active.' ), $signup );
+
+        return new GFSignup( $signup );
+    }
+
+    function get_activation_type() {
+        return rgars( $this->config, 'meta/user_activation_type' );
+    }
+
+    function set_as_activated() {
+        global $wpdb;
+
+        $now = current_time('mysql', true);
+        $result = $wpdb->update( $wpdb->signups, array( 'active' => 1, 'activated' => $now ), array( 'activation_key' => $this->activation_key ) );
+
+        return $result;
+    }
+
+}
+
+?>
