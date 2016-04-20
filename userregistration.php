386fc3f18a70e1f616100699c7253e042eee2b25
Gravity Forms <info@gravityforms.com>
2016-04-20 13:56:33 +0200

    3.3

--- userregistration.php
+++ userregistration.php
@@ -1,9 +1,9 @@
 <?php
-/*
+/**
 Plugin Name: Gravity Forms User Registration Add-On
 Plugin URI: http://www.gravityforms.com
 Description: Allows WordPress users to be automatically created upon submitting a Gravity Form
-Version: 3.2.1
+Version: 3.3
 Author: rocketgenius
 Author URI: http://www.rocketgenius.com
 Text Domain: gravityformsuserregistration
@@ -25,15 +25,27 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-*/
+**/
 
-define( 'GF_USER_REGISTRATION_VERSION', '3.2.1' );
+define( 'GF_USER_REGISTRATION_VERSION', '3.3' );
 
+// If Gravity Forms is loaded, bootstrap the User Registration Add-On.
 add_action( 'gform_loaded', array( 'GF_User_Registration_Bootstrap', 'load' ), 5 );
 
+/**
+ * Class GF_User_Registration_Bootstrap
+ *
+ * Handles the loading of the User Registration add-on and registers with the add-on framework
+ */
 class GF_User_Registration_Bootstrap {
 
-	public static function load(){
+	/**
+	 * If the Feed Add-On Framework exists, User Registration Add-On and login widget are loaded.
+	 *
+	 * @access public
+	 * @static
+	 */
+	public static function load() {
 
 		if ( ! method_exists( 'GFForms', 'include_feed_addon_framework' ) ) {
 			return;
@@ -48,63 +60,22 @@ class GF_User_Registration_Bootstrap {
 
 }
 
+/**
+ * Returns an instance of the GF_User_Registration class
+ *
+ * @see    GF_User_Registration::get_instance()
+ * @return object GF_User_Registration
+ */
 function gf_user_registration() {
 	return GF_User_Registration::get_instance();
 }
 
+/**
+ * Obtains the login form HTML markup
+ *
+ * @see    GF_User_Registration->get_login_html()
+ * @return string The login form HTML
+ */
 function gf_user_registration_login_form() {
 	return gf_user_registration()->get_login_html();
-}
-
-if ( ! function_exists( 'wp_new_user_notification' ) ) {
-// ----------- Forked from WP 4.4.1, restoring the $plaintext_pass param which was deprecated in WP 4.3.1. ---------------
-
-	function wp_new_user_notification( $user_id, $plaintext_pass = '', $notify = '' ) {
-		global $wpdb, $wp_hasher;
-		$user = get_userdata( $user_id );
-
-		// The blogname option is escaped with esc_html on the way into the database in sanitize_option
-		// we want to reverse this for the plain text arena of emails.
-		$blogname = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
-
-		$message = sprintf( __( 'New user registration on your site %s:' ), $blogname ) . "\r\n\r\n";
-		$message .= sprintf( __( 'Username: %s' ), $user->user_login ) . "\r\n\r\n";
-		$message .= sprintf( __( 'Email: %s' ), $user->user_email ) . "\r\n";
-
-		$result = @wp_mail( get_option( 'admin_email' ), sprintf( __( '[%s] New User Registration' ), $blogname ), $message );
-		gf_user_registration()->log_wp_mail( $result, 'admin' );
-
-		if ( 'admin' === $notify || ( empty( $plaintext_pass ) && empty( $notify ) ) ) {
-			return;
-		}
-
-		$message = sprintf( __( 'Username: %s' ), $user->user_login ) . "\r\n\r\n";
-
-		if ( empty( $plaintext_pass ) ) {
-			// Generate something random for a password reset key.
-			$key = wp_generate_password( 20, false );
-
-			/** This action is documented in wp-login.php */
-			do_action( 'retrieve_password_key', $user->user_login, $key );
-
-			// Now insert the key, hashed, into the DB.
-			if ( empty( $wp_hasher ) ) {
-				require_once ABSPATH . WPINC . '/class-phpass.php';
-				$wp_hasher = new PasswordHash( 8, true );
-			}
-			$hashed = time() . ':' . $wp_hasher->HashPassword( $key );
-			$wpdb->update( $wpdb->users, array( 'user_activation_key' => $hashed ), array( 'user_login' => $user->user_login ) );
-
-
-			$message .= __( 'To set your password, visit the following address:' ) . "\r\n\r\n";
-			$message .= '<' . network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user->user_login ), 'login' ) . ">\r\n\r\n";
-		} else {
-			$message .= sprintf( __( 'Password: %s' ), $plaintext_pass ) . "\r\n\r\n";
-		}
-
-		$message .= wp_login_url() . "\r\n";
-
-		$result = wp_mail( $user->user_email, sprintf( __( '[%s] Your username and password info' ), $blogname ), $message );
-		gf_user_registration()->log_wp_mail( $result, 'user' );
-	}
 }
